ea9d8356ccc71fa44bd78efc49454d6fac8aa226
OracunPB <paubs1@hotmail.com>
2024-12-20 08:39:07 +0100

    FINAL LAST FANTASY UPDATE 24 with comentarios

--- companyia_aerea.sql
+++ companyia_aerea.sql
@@ -10,6 +10,16 @@
 -- Creació de les taules
 
 drop table if exists companyia;
+drop table if exists avio;
+drop table if exists aeroport;
+drop table if exists mostrador;
+drop table if exists personal;
+drop table if exists hostessa;
+drop table if exists pilot;
+drop table if exists passatger;
+drop table if exists vol;
+drop table if exists volar;
+
 create table companyia (
     nom varchar (40) not null, /* primary key */
     IATA varchar (6) not null,
@@ -19,7 +29,6 @@ create table companyia (
     filial_de varchar (40) /* foreign key REFERENCES companyia(nom) */
 );
 
-drop table if exists avio;
 create table avio (
     num_serie varchar (30) not null, /* primary key */
     tipus varchar (10) not null, /* (comercial, passatgers, transport de mercaderies) */
@@ -28,7 +37,6 @@ create table avio (
     companyia varchar (40) not null /* foreign key REFERENCES companyia(nom) */
 );
 
-drop table if exists aeroport;
 create table aeroport (
     codi varchar (4) not null, /* primary key */
     pais varchar (40) not null,
@@ -38,15 +46,11 @@ create table aeroport (
     any_construccio year
 );
 
-drop table if exists mostrador;
 create table mostrador (
     numero smallint unsigned not null, /* primary key */
     codi_aeroport varchar (4) not null /* foreign key REFERENCES aeroport(codi) */
 );
 
-
-
-drop table if exists personal;
 create table personal (
     num_empleat int unsigned not null,
     nom varchar (25) not null,
@@ -55,18 +59,15 @@ create table personal (
     sou float not null
 );
 
-drop table if exists hostessa;
 create table hostessa (
     num_empleat int unsigned not null
 );
 
-drop table if exists pilot;
 create table pilot (
     hores_vol smallint unsigned,
     num_empleat int unsigned not null
 );
 
-drop table if exists passatger;
 create table passatger (
     passaport varchar (20) not null,
     nom varchar (30) not null,
@@ -78,7 +79,6 @@ create table passatger (
     genere char (1)
 );
 
-drop table if exists vol;
 create table vol (
     codi varchar (9) not null,
     data date not null,
@@ -86,7 +86,6 @@ create table vol (
     descripcio varchar (15)
 );
 
-drop table if exists volar;
 create table volar (
     passatger varchar (20) not null,
     vol varchar (9) not null,
